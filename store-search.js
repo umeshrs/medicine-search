344a775a4bf740063a9e16e0d3e0c0d8016c2171
Umesh <rsu1991@gmail.com>
2015-10-13 12:37:38 +0530

    Added event handler for file upload form which parses the uploaded csv file and populates the medicines collection. Created sample csv file to test file upload.

--- store-search.js
+++ store-search.js
@@ -3,7 +3,46 @@ Medicines = new Mongo.Collection("medicines");
 if (Meteor.isClient) {
   Template.medicinesList.helpers({
     medicines: function () {
-      return Medicines.find({});
+      return Medicines.find({}, { sort: { name: 1 } });
+    }
+  });
+
+  Template.fileUploadForm.events({
+    'click #upload-button': function () {
+      var config, i, medicineDetails, key;
+
+      config = {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+          console.log(results);
+
+          for (i = 0; i < results.data.length; i++) {
+            medicineDetails = results.data[i];
+
+            Medicines.insert({
+              name: medicineDetails['medicineName'],
+              purpose: medicineDetails['purpose'],
+              createdAt: new Date()
+            });
+          }
+
+        }
+      };
+
+      $('#file-input').parse({
+        config: config,
+        before: function (file, inputElem) {
+          console.log("Parsing file...", file);
+        },
+        error: function (err, file) {
+          console.log("ERROR:", err, file);
+        },
+        complete: function (results) {
+          console.log("Parsing complete.");
+        }
+      });
     }
   });
 }
